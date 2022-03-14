d91edafa3d8717357098fb3d9e39f3d34ae84242
Olivier_Luethy <olivier@kauz.ch>
2022-03-14 16:56:59 +0100

    Restart of the game

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Hangyman-in-JS
+# Hangyman made with JavaScript
+Ein Hangman Spiel bassierend zu meinem Spiel, welches ich mit Discord.py gemacht habe.
+
+## Vorgekommene Fehler und gute Links
+Issue 1: How to detect which button has been clicked in JavaScript<br>
+https://www.fwait.com/how-to-detect-which-button-is-clicked-in-javascript/
--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    window.onload = function () {
+var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var words = ['abandon', 'ability', 'able', 'abortion', 'about', 'above', 'abroad', 'absence', 'absolute', 'absorb', 'bill', 'billion', 'bag', 'approximately'];
+var chosenLetter = "";
+var word = words[Math.floor(Math.random() * words.length)];
 
-        var words = ['Apple', 'Banana'];
+document.addEventListener('DOMContentLoaded', function() {
+    window.onload = function() {
 
-        var word = words[Math.floor(Math.random() * words.length)];
-
-        for (let i = 0; i < word.length; i++) {
-            createInputfield();
+        // Generates all the buttons
+        for (i = 0; i < alphabet.length; i++) {
+            var button = document.createElement("button");
+            var textnode = document.createTextNode(alphabet[i]);
+            button.appendChild(textnode);
+            document.getElementById("alphabeticButtons").appendChild(button);
         }
 
-        document.getElementById("guess").value = "My value";
+        for (let i = 0; i < word.length; i++) {
+            var input = document.createElement("input");
+            document.getElementById("inputFields").appendChild(input);
+        }
     }
 });
 
-function createInputfield() {
-    var input = document.createElement("INPUT");
-    var textnode = document.createTextNode("lul");
-    input.appendChild(textnode);
-    document.getElementById("information").appendChild(input);
-    input.setAttribute("id", "guessLetter");
-}
+document.addEventListener("click", (e) => {
+    let element = e.target;
+    if (element.tagName == "BUTTON") {
+        chosenLetter = `${element.innerText}`;
+        words = Array.from(word);
+        for (let i = 0; i < words.length; i++) {
+            if (words[i] == chosenLetter) {
+                alert("x3");
+            }
+        }
+    } else {
+
+    }
+})
