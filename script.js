b6cb6f7efde0f6025a90e8be8364b1ce91fedcc4
Olivier_Luethy <olivier@kauz.ch>
2022-03-15 14:24:13 +0100

    Responsive Design

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+/* Array with letters from A-Z for Buttons */
 var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+/* The array with words, where one word will be taken for the game */
 let words = ["ABART", "AFFIG", "AFTER", "BADEN", "BAMBI", "PEDAL", "PERLE", "PFAHL", "PFAND", "PFEIL", "PFERD", "PFLUG", "PFOTE", "PFUND", "PHASE", "PIANO", "PILLE", "PILOT", "PIRAT", "PISTE", "PIXEL", "PLAGE", "PLATZ", "POKAL", "POKER", "POREN", "PREIS", "PRINZ", "PUDER", "PUMPE", "PUPPE", "PUSTE", "QUALM", "QUARZ", "RACHE", "RADAU", "RADIO", "RAMPE", "RANKE", "RATTE", "RAUCH", "RAUPE", "RECHT", "REGAL", "REGEL", "REGEN"];
+/* Stores the chosen letter from the user */
 var chosenLetter = "";
+/* Picks a random word from the word-array */
 var word = words[Math.floor(Math.random() * words.length)];
-word = word.toUpperCase();
+/* Is used for the modal to show, what the hidden word was */
 document.getElementById("word").innerHTML = "The word was " + word;
+/* Turn picked word from string to array */
 arrayWord = Array.from(word);
+/* Get length of random word-array */
 var wordLength = arrayWord.length;
+/* To get the number of used attemps */
 var attemps = 0;
+/* To determine if user guessed correctly */
 var lucky = false;
+/* To check if user has lost the game or not */
 var lost = false;
 
+/* When the website has been loaded */
 document.addEventListener('DOMContentLoaded', function() {
     window.onload = function() {
 
@@ -21,6 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById("alphabeticButtons").appendChild(button);
         }
 
+        /* Generates the number of input field == number of letters in generated word */
         for (let i = 0; i < word.length; i++) {
             var input = document.createElement("input");
             input.readOnly = true;
@@ -29,36 +40,47 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+/* If something has been clicked */
 document.addEventListener("click", (e) => {
+    /* Check if player already lost the game */
     if (lost == false) {
         let element = e.target;
+        /* Check if clicked element is a button */
         if (element.tagName == "BUTTON") {
+            /* If yes, button gets some new properties */
             element.style.backgroundColor = "black";
             element.style.setProperty("text-decoration", "line-through");
+            /* Get text from button */
             chosenLetter = `${element.innerText}`;
+            /* Check if the array contains an element that is the same as the letter of the variable */
             if (arrayWord.includes(chosenLetter)) {
+                /* If it does, determine the index of it */
                 for (let i = 0; i < arrayWord.length; i++) {
                     if (arrayWord[i] == chosenLetter) {
+                        /* If the value from index is the same a the letter from variable, */
+                        /* set the value from input field by using nth-child to fill the correct input field */
                         document.querySelector("#inputFields input:nth-child(" + (i + 1) + ")").value = arrayWord[i];
                     }
                 }
-                if (wordLength == 0) {
-                    winningmodal.style.display = "block";
-                }
                 wordLength--;
-                lucky = true;
 
+                /* Check if user already guessed the entire word */
                 if (wordLength == 0) {
                     winningmodal.style.display = "block";
                 }
-            } else {
+                lucky = true;
+            }
+            /* If the letter isn't in array, the user guessed a wrong */
+            else {
                 lucky = false;
             }
 
+            /* Check if user has guessed the wrong letter */
             if (lucky == false) {
                 attemps++;
                 console.log(word);
 
+                /* Is reponsible for the hangman image on the left of the game */
                 if (attemps == 1) {
                     document.querySelector(".thedraw").style.display = "block";
                 } else if (attemps == 2) {
@@ -74,16 +96,22 @@ document.addEventListener("click", (e) => {
                 } else if (attemps == 7) {
                     document.querySelector(".theman .hands").style.display = "block";
                 } else if (attemps == 8) {
+                    /* If the user has already used 8 guesses, the last part of the body */
+                    /* is shown and the loser-Modal will be displayed */
                     document.querySelector(".theman .legs").style.display = "block";
                     lostmodal.style.display = "block";
                     lost = true;
                 }
-                lucky = false;
             }
         }
-    } else if (lost == true) {
+    }
+    /* If user has lost the game */
+    else if (lost == true) {
+        /* Get an array from all the buttons */
         var button = document.getElementsByTagName("BUTTON");
+        /* Go through all the buttons in array */
         for (var i = 0; i < button.length; i++) {
+            /* If the button isn't disabled he will be disabled and get some different properties */
             if (button[i].disabled == false) {
                 button[i].style.backgroundColor = "gray";
                 button[i].style.setProperty("text-decoration", "none");
@@ -94,6 +122,8 @@ document.addEventListener("click", (e) => {
     }
 })
 
+/* Shown modal when you have won the game */
+/* -------------------------------------- */
 // Get the modal when you have won
 var winningmodal = document.getElementById("winningModal");
 
@@ -112,6 +142,8 @@ window.onclick = function(event) {
     }
 }
 
+/* Shown modal when you have lost the game */
+/* --------------------------------------- */
 // Get the modal when you have lost
 var lostmodal = document.getElementById("lostModal");
 
@@ -126,6 +158,6 @@ span.onclick = function() {
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == lostmodal) {
-        lostmodalmodal.style.display = "none";
+        lostmodal.style.display = "none";
     }
 }
